717f7c9887e2683c0d57787e6e0f57b094d7bc0a
João Victor Sant'ana <viitorsant@gmail.com>
2022-08-13 20:05:36 -0300

    remove node_modules e dist

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import './app.css'
 import App from './App.svelte'
 
+import { registerSW } from "virtual:pwa-register";
+
 const app = new App({
   target: document.getElementById('app')
 })
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
 
 export default app
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    VitePWA({
+      workbox: {
+          globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
+      },
+      injectRegister: 'auto',
+      registerType: "autoUpdate",
+      manifest: {
+        "background_color": "#ffffff",
+        "theme_color": "#ffffff",
+        "name": "Lista de Compras",
+        "short_name": "Listpicker",
+        "display": "standalone",
+        "icons": []
+    }
+    })
+  ]
 })
