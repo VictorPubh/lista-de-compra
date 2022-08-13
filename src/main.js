b06bdab03f130f4d1e0efe941a89c02640b37a05
João Victor Sant'ana <viitorsant@gmail.com>
2022-08-13 20:07:37 -0300

    remove registerSW

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import './app.css'
 import App from './App.svelte'
 
-import { registerSW } from "virtual:pwa-register";
-
 const app = new App({
   target: document.getElementById('app')
 })
-
-if ("serviceWorker" in navigator) {
-  // && !/localhost/.test(window.location)) {
-  registerSW();
-}
 
 export default app
